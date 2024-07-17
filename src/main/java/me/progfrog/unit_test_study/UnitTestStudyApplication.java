db16f8c98d2075ea5bfe614c0ccdb392a6db74e9
happyprogfrog <happyprogfrog@gmail.com>
2024-07-17 09:36:43 +0900

    프로젝트 생성

---
+++ src/main/java/me/progfrog/unit_test_study/UnitTestStudyApplication.java
@@ -0,0 +1,13 @@
+package me.progfrog.unit_test_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/progfrog/unit_test_study/UnitTestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package me.progfrog.unit_test_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
